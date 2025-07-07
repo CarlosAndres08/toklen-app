b3bc9aaeef2216a74bee758180ed90d6c1bff480
CarloAndres08 <carlosandres08@gmail.com>
2025-07-07 15:57:34 -0500

    Tu mensaje aquí, por ejemplo: Actualización de frontend

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-mport React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useAuth } from '../hooks/useAuth'
 import { useNavigate } from 'react-router-dom'
 import { 
