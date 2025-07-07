9c54c8e526a3d13fdafb13e01d66342d7b8147d8
CarloAndres08 <carlosandres08@gmail.com>
2025-07-07 17:45:16 -0500

    Tu mensaje aquí, por ejemplo: Actualización de frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { StrictMode } from 'react'
 import App from './App.jsx'
-import './styles/globals.css'
+
 import './styles/index.css';
 
 // Configurar variables de entorno en modo desarrollo
