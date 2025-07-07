74faa0fee165301204c8a85cc0e54148786182d0
CarloAndres08 <carlosandres08@gmail.com>
2025-07-07 15:46:46 -0500

    Tu mensaje aquí, por ejemplo: Actualización de frontend

--- frontend/src/pages/SearchProfessionals.jsx
+++ frontend/src/pages/SearchProfessionals.jsx
@@ -1,4 +1,4 @@
-mport React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { professionalsAPI } from '../services/api'
 import { SERVICE_CATEGORIES, CATEGORY_LABELS, LIMA_DISTRICTS } from '../utils/constants'
